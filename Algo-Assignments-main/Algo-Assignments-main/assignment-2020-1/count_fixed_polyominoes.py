b4949275a226fa93be15047cd101722060606c4e
epanourgia <eva.panourgiaa@gmail.com>
2022-03-01 19:08:26 +0200

    Produce possible solution neighbourhood based on black line

---
+++ Algo-Assignments-main/Algo-Assignments-main/assignment-2020-1/count_fixed_polyominoes.py
@@ -0,0 +1,68 @@
+""""
+Strategy 
+1. filter - Create Graph 
+2. Implement pseudocode 
+"""
+
+# import libaries 
+
+
+def CountFixedPolynoes(G, untried, n, p, c):
+    """
+    Input :
+    - G, G = (V, E), a graph
+    - untried, a set of nodes
+    - n, the size polyominos    
+    - p, the current polynomino 
+    - c, a counter
+    Output : 
+    - r, the number of polynominoes of size n
+    """
+
+
+    pass
+
+def fix_graph(n):
+    """Fix graph based on the size polynominos given from terminan(cmd)"""
+    begin = (0,0)
+    horizontally_right = [] 
+    intermediate = (0,1)
+    horizontally_left = [] 
+
+    for i in range(n):
+        horizontally_right.append((i,0))
+    
+    for i in range(n-1):
+        result_x = intermediate[0] - i
+        horizontally_left.append((result_x,1))
+    list_border = horizontally_right + horizontally_left
+    list_border_backup = []
+    # produce possible solution space 
+    for i in range(len(list_border)):
+        creator = list_border[i]
+        if creator[1] == 0: 
+            for i in range(n): # 5 
+                list_border_backup.append((creator[0], creator[1]+i))    
+        elif creator[1] == 1:
+            for i in range(n-1):
+                if creator == (0,1):
+                    continue
+                list_border_backup.append((creator[0], creator[1]+i))
+    print(list_border_backup)
+
+    # filter
+    #create final graph 
+    list_all_nodes = list_border.copy()
+
+    exit()
+    return G
+    
+
+## Initialize parameters to call the main function 
+n = 5 
+G = fix_graph(n)
+print(G)
+# untried={(0,0)}
+# p = []
+# # call main function that solve the problem 
+# CountFixedPolynoes(G, untried, n, p, c=0)
