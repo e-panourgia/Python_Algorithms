322a7598fad87546944f485f93578301a56f7090
epanourgia <eva.panourgiaa@gmail.com>
2022-03-01 20:17:15 +0200

    Create G - graph

--- Algo-Assignments-main/Algo-Assignments-main/assignment-2020-1/count_fixed_polyominoes.py
+++ Algo-Assignments-main/Algo-Assignments-main/assignment-2020-1/count_fixed_polyominoes.py
@@ -1,11 +1,5 @@
-""""
-Strategy 
-1. filter - Create Graph 
-2. Implement pseudocode 
-"""
-
 # import libaries 
-
+import pprint as pp
 
 def CountFixedPolynoes(G, untried, n, p, c):
     """
@@ -22,7 +16,7 @@ def CountFixedPolynoes(G, untried, n, p, c):
 
     pass
 
-def fix_graph(n):
+def produce_nodes(n):
     """Fix graph based on the size polynominos given from terminan(cmd)"""
     begin = (0,0)
     horizontally_right = [] 
@@ -38,12 +32,11 @@ def fix_graph(n):
     list_border = horizontally_right + horizontally_left
     list_border_backup = []
     # produce possible solution space 
-    k = n
-    m = n
-    for i in range(len(list_border)): ## all meria
+    k, m = n, n
+    for i in range(len(list_border)): 
         creator = list_border[i]
         if creator[1] == 0: 
-            for i in range(k): # all n first colona
+            for i in range(k): 
                 list_border_backup.append((creator[0], creator[1]+i))
             k -= 1    
     
@@ -53,20 +46,50 @@ def fix_graph(n):
                     continue
                 list_border_backup.append((creator[0], creator[1]+i))
             m -= 1
-    print(list_border_backup)
+    return list_border_backup
 
-    # filter
-    #create final graph 
-    #list_all_nodes = list_border.copy()
-
-    exit()
-    #return G
+def create_graph(list_nodes):
+    # sort keys to find them with the proper order
+    list_nodes.sort(key=lambda y: y[0])
+    hold_neigbours = []
+    # create connectors 
+    for i in range(len(list_nodes)):
+        list_tuple = []
+        x = list_nodes[i][0]
+        y = list_nodes[i][1]
+        x_plus = x + 1
+        x_minus = x - 1
+        y_plus = y + 1
+        y_minus = y - 1
+        # possible tuple to check 
+        tuple_1 = (x_minus, y)
+        tuple_2 = (x_plus, y) 
+        tuple_3 = (x, y_minus)
+        tuple_4 = (x, y_plus)
+        list_tuple.append(tuple_1)
+        list_tuple.append(tuple_2)
+        list_tuple.append(tuple_3)
+        list_tuple.append(tuple_4)
+        sub_list = []
+        for f in range(len(list_tuple)):
+           if list_tuple[f] in list_nodes:
+                sub_list.append(list_tuple[f]) 
+        hold_neigbours.append(sub_list)
     
+    return hold_neigbours,list_nodes
 
+def create_final_graph(neighbours, keys):
+    G = {} # Initialize python-dict
+    for i in range(len(keys)):
+        G[keys[i]] = neighbours[i]
+    return G
 ## Initialize parameters to call the main function 
 n = 5 
-G = fix_graph(n)
-print(G)
+list_nodes = produce_nodes(n)
+neighbours, keys = create_graph(list_nodes)
+G = create_final_graph(neighbours, keys)
+pp.pp(G)
+# print(G)
 # untried={(0,0)}
 # p = []
 # # call main function that solve the problem 
